b28daf9d4df55794033065e32db5e5e86ef999c0
maxamed1st <maxamed1st@icloud.com>
2022-04-12 18:35:36 +0200

    Create operator object that performs basic arithmetic

--- calculator.js
+++ calculator.js
@@ -10,7 +10,7 @@ const subtraction = document.getElementById("subtraction");
 const clear = document.getElementById("clear");
 const allClear = document.getElementById("allClear");
 
-//Screen object
+//Screen object to view and accumulate inputs to the calculator
 const screen = {
     allClear : function() {
         currentValue.textContent = "";
@@ -20,6 +20,20 @@ const screen = {
     updateCurrent : value => currentValue.textContent = value,
     accumulate : value => accumulatorScreen.textContent += `${value} `
 };
-
+//Operator object performs the basic arithmetic operations and produces result
+const operator = {
+    multiplication : (num1, num2) => {
+        return num1*num2;
+    },
+    division : (num1, num2) => {
+        return num1/num2;
+    },
+    addition : (num1, num2) => {
+        return num1*num2;
+    },
+    subtraction : (num1, num2) => {
+        return num1*num2;
+    }
+}
 allClear.onmouseup = screen.allClear;
 clear.onmouseup = screen.clear;
